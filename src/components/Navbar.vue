eb4c737c30470153c638ff4a5825cf0fa52ab8a4
James Larson <timothy_james@tuta.io>
2025-01-28 21:40:20 -0800

    chore: update name

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar bg-base-100">
     <div class="flex-1">
-      <router-link :to="'/home'" class="btn btn-ghost prose text-3xl">The Neutral Times</router-link>
+      <router-link :to="'/home'" class="btn btn-ghost prose text-3xl">The Measured Gazette</router-link>
       <div class="flex w-full flex-col">
         <div class="divider"></div>
       </div>
