912935dfc82968071727474d55da517d412755f2
James Larson <timothy_james@tuta.io>
2025-02-03 03:05:14 +0000

    chore: update to new domain with better security

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,7 @@ import type { Category } from './data/category'
 import type { Article } from './data/article'
 
 const api = axios.create({
-  baseURL: 'https:/neutralnews.dev',
+  baseURL: 'https://staging.api.measured-gazette.com/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
@@ -13,18 +13,18 @@ const api = axios.create({
 }) as ApiInstance
 
 api.fetchArticle = async (id: number): Promise<Article> => {
-  const response = await api.get<Article>(`v2/articles/${id}?api_key=asdfasdfasdf`)
+  const response = await api.get<Article>(`v2/articles/${id}`)
   return response.data
 }
 
 api.fetchCategories = async (): Promise<Category[]> => {
-  const response = await api.get<ApiResponse<Category[]>>('v2/categories?api_key=asdfasdfasdf')
+  const response = await api.get<ApiResponse<Category[]>>('v2/categories')
   return response.data.data
 }
 
 api.fetchCategoryArticles = async (category: Category): Promise<Article[]> => {
   const response = await api.get<ApiResponse<Article[]>>(
-    `v2/categories/${category.id}/articles?api_key=asdfasdfasdf`
+    `v2/categories/${category.id}/articles`
   )
   return response.data.data
 }
